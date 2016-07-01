fa5fe7559da901c43a416c4ef79f311c22021f20
Zach Gaines <zrgaines@mtu.edu>
2016-07-01 16:20:32 -0500

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,18 @@ var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DateHub' });
 });
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET signup page */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 
 module.exports = router;
