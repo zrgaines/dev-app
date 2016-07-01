4307b63845f903624e74dd90cfcf820e422ea160
Robert Reilley <rpreilley@gmail.com>
2016-07-01 16:23:18 -0500

    commit before merge conflicts fix

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next){
+  // redirect to index
+  res.redirect('/')
+  });
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/signup', function(req, res, next){
+  // redirect to index
+  res.redirect('/')
+  });
+
+
 module.exports = router;
